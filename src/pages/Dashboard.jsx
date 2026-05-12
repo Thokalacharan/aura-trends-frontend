ce2f3c660ed8cea25116d86581021d016e6d46c7
Thokalacharan <thokalacharan40@gmail.com>
2026-05-12 09:14:17 +0530

    fixed create task form submission

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -157,14 +157,13 @@ function Dashboard() {
 
             <h1>Create Task</h1>
 
-            <form className="task-form">
-              <form
+            <form
   className="task-form"
   onSubmit={(e) => {
     e.preventDefault();
     alert("Task Created Successfully");
   }}
-></form>
+>
 
               <input type="text" placeholder="Task Title" />
 
